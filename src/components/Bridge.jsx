5420f83db4177bee5a5115a0d1d649f70bf1bfc3
apoorveth <95699312+apoorveth@users.noreply.github.com>
2023-03-17 16:36:29 +0530

    added bridge screen

--- src/components/Bridge.jsx
+++ src/components/Bridge.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import DownArrow from '../assets/img/down_arrow.svg';
+import TokenSelector from './TokenSelector.jsx';
+import faucetTokens from '../config/faucetTokens.json';
 
 const BridgeContainer = styled.div`
     display: flex;
@@ -17,13 +20,13 @@ const WalletAddress = styled.div`
     font-family: GilmerHeavy;
     font-size: 0.9rem;
     border: 1px solid rgb(54 53 53);
+    margin-bottom: 5%;
 `;
 
 const CoinSelectorContainer = styled.div`
     display: flex;
     flex-direction: column;
-    padding-left: 2%;
-    margin-top: 5%;
+    cursor: pointer;
 `;
 
 const CoinSelectorHeading = styled.div`
@@ -34,43 +37,116 @@ const CoinSelectorHeading = styled.div`
 
 const CoinSelectorInputContainer = styled.div`
     background-color: #121212;
-    padding-top: 2%;
-    padding-bottom: 2%;
-    border-radius: 50px;
+    padding-top: 3%;
+    padding-bottom: 3%;
+    border-radius: 15px;
     border: 1px solid rgb(54 53 53);
     padding-left: 5%;
     margin-top: 2%;
     display: flex;
     flex-direction: row;
+    justify-content: space-between;
 `;
 
 const CoinSelectorInputContainerAmount = styled.input`
-    width: 70%;
+    width: 100%;
+    padding-right: 5%;
     outline: none;
     background-color: transparent;
     color: white;
     font-family: GilmerHeavy;
     font-size: 1.3rem;
     border: 0;
+
+    &::-webkit-inner-spin-button,
+    &::-webkit-outer-spin-button {
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+        margin: 0;
+    }
 `;
 
 const CoinSelectorInputContainerCoin = styled.div`
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
     justify-content: center;
     align-items: center;
+    margin-right: 4%;
 `;
 
-const CoinImage = styled.img``;
+const CoinName = styled.div`
+    color: white;
+    font-family: GilmerMedium;
+    margin-right: 10px;
+    font-size: 1.2rem;
+`;
 
-const CoinSelector = () => {
+const CoinAndChainImageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    position: relative;
+`;
+
+const CoinImage = styled.img`
+    height: 40px;
+    background-color: white;
+    border-radius: 50px;
+`;
+
+const ChainImage = styled.img`
+    height: 17px;
+    position: absolute;
+    border: 1px solid black;
+    bottom: 0;
+    left: -15%;
+    background-color: white;
+    border-radius: 50px;
+    border: 1px solid black;
+    bottom: -15%;
+`;
+
+const SwapArrow = styled.img`
+    height: 25px;
+    margin-top: 15px;
+`;
+
+const SubmitButton = styled.div`
+    width: 100%;
+    border-radius: 50px;
+    background-color: #6de573;
+    color: black;
+    font-family: GilmerMedium;
+    margin: auto;
+    text-align: center;
+    padding-top: 4%;
+    padding-bottom: 4%;
+    font-size: 1.3rem;
+    margin-top: 8%;
+    cursor: pointer;
+`;
+
+const CoinSelector = ({
+    heading,
+    coinLogo,
+    chainLogo,
+    symbol,
+    onTokenClick,
+}) => {
     return (
         <CoinSelectorContainer>
-            <CoinSelectorHeading>From:</CoinSelectorHeading>
+            <CoinSelectorHeading>{heading}</CoinSelectorHeading>
             <CoinSelectorInputContainer>
-                <CoinSelectorInputContainerAmount></CoinSelectorInputContainerAmount>
-                <CoinSelectorInputContainerCoin>
-                    <CoinImage src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fen%2Fsearch%3Fq%3Dethereum&psig=AOvVaw0UquGxvOewNvDA7Bz0KnH-&ust=1678978770029000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCMiNlLuZ3v0CFQAAAAAdAAAAABAD"></CoinImage>
+                <CoinSelectorInputContainerAmount
+                    type="number"
+                    placeholder="11.2"
+                ></CoinSelectorInputContainerAmount>
+                <CoinSelectorInputContainerCoin onClick={onTokenClick}>
+                    <CoinName>{symbol}</CoinName>
+                    <CoinAndChainImageContainer>
+                        <CoinImage src={coinLogo}></CoinImage>
+                        <ChainImage src={chainLogo}></ChainImage>
+                    </CoinAndChainImageContainer>
                 </CoinSelectorInputContainerCoin>
             </CoinSelectorInputContainer>
         </CoinSelectorContainer>
@@ -78,10 +154,76 @@ const CoinSelector = () => {
 };
 
 const Bridge = ({ wallet }) => {
+    const [isTokenSelectorOpen, setIsTokenSelectorOpen] = useState(false);
+    const [lastTokenSelector, setLastTokenSelector] = useState('');
+    const [fromState, setFromState] = useState({
+        token: 'ethereum',
+        chain: 'ethereum',
+    });
+    const [toState, setToState] = useState({
+        token: 'ethereum',
+        chain: 'ethereum',
+    });
+
+    const fromTokenClick = () => {
+        setLastTokenSelector('from');
+        setIsTokenSelectorOpen(true);
+    };
+
+    const toTokenClick = () => {
+        setLastTokenSelector('to');
+        setIsTokenSelectorOpen(true);
+    };
+
+    const closeTokenSelector = () => {
+        setIsTokenSelectorOpen(false);
+    };
+
+    const onTokenSelect = (state) => {
+        switch (lastTokenSelector) {
+            case 'from':
+                setFromState(state);
+                break;
+            case 'to':
+                setToState(state);
+                break;
+        }
+        closeTokenSelector();
+    };
+
     return (
         <BridgeContainer>
-            <WalletAddress>{wallet.address}</WalletAddress>
-            <CoinSelector></CoinSelector>
+            {/* <WalletAddress>{wallet.address}</WalletAddress> */}
+            {isTokenSelectorOpen ? (
+                <TokenSelector
+                    onTokenSelect={onTokenSelect}
+                    close={closeTokenSelector}
+                    chain={
+                        lastTokenSelector == 'from'
+                            ? fromState.chain
+                            : toState.chain
+                    }
+                ></TokenSelector>
+            ) : (
+                <>
+                    <CoinSelector
+                        heading="From:"
+                        coinLogo={faucetTokens.tokens[fromState.token].image}
+                        chainLogo={faucetTokens.chains[fromState.chain].image}
+                        symbol={faucetTokens.tokens[fromState.token].symbol}
+                        onTokenClick={fromTokenClick}
+                    ></CoinSelector>
+                    <SwapArrow src={DownArrow}></SwapArrow>
+                    <CoinSelector
+                        heading="To:"
+                        coinLogo={faucetTokens.tokens[toState.token].image}
+                        chainLogo={faucetTokens.chains[toState.chain].image}
+                        symbol={faucetTokens.tokens[toState.token].symbol}
+                        onTokenClick={toTokenClick}
+                    ></CoinSelector>
+                    <SubmitButton>Get Gas</SubmitButton>
+                </>
+            )}
         </BridgeContainer>
     );
 };
---
+++ src/components/TokenSelector.jsx
@@ -0,0 +1,155 @@
+import React, { useEffect, useState } from 'react';
+import styled, { css } from 'styled-components';
+import faucetTokens from '../config/faucetTokens.json';
+import crossLogo from '../assets/img/cross.png';
+
+const TokenSelectorContainer = styled.div`
+    background-color: #121212;
+    padding: 4%;
+    border-radius: 10px;
+    border: 1px solid #1c1c1c;
+    position: relative;
+`;
+
+const Heading = styled.div`
+    color: white;
+    font-family: GilmerMedium;
+    font-size: 1.1rem;
+`;
+
+const ChainSelectorContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    margin-top: 2%;
+    justify-content: space-between;
+    padding-right: 10%;
+`;
+
+const Chain = styled.div`
+    background-color: ${(props) => (props.selected ? '#6DE573' : '#1c1c1c')};
+    padding-top: 2%;
+    padding-bottom: 2%;
+    padding-left: 4%;
+    padding-right: 4%;
+    border-radius: 50px;
+    margin-bottom: 2%;
+    color: ${(props) => (props.selected ? 'black' : 'white')};
+
+    &:hover {
+        background-color: ${(props) =>
+            props.selected ? '#6DE573' : '#3e3e3e'};
+    }
+    cursor: pointer;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+`;
+
+const ChainImage = styled.img`
+    width: 20px;
+    background-color: white;
+    border-radius: 50px;
+`;
+
+const ChainName = styled.div`
+    font-size: 1rem;
+    margin-left: 5px;
+`;
+
+const TokeRow = styled.div`
+    display: flex;
+    flex-direction: row;
+    color: ${(props) => (props.live ? 'white' : 'grey')};
+    border-radius: 15px;
+    padding: 2%;
+    margin-top: 2%;
+    align-items: center;
+    cursor: pointer;
+    &:hover {
+        background-color: #3e3e3e;
+    }
+
+    ${(props) => {
+        if (!props.live) {
+            return css`
+                pointer-events: none;
+            `;
+        }
+    }}
+`;
+
+const TokenImage = styled.img`
+    width: 40px;
+    background-color: white;
+    border-radius: 50px;
+`;
+
+const TokenName = styled.div`
+    font-size: 1.1rem;
+    margin-left: 4%;
+    font-family: GilmerMedium;
+`;
+
+const TokenComingSoon = styled.div`
+    font-size: 0.9rem;
+    margin-left: 4%;
+    font-family: GilmerMedium;
+    color: grey;
+    margin-left: auto;
+`;
+
+const CloseButton = styled.img`
+    position: absolute;
+    top: 3%;
+    right: 3%;
+    width: 12px;
+    cursor: pointer;
+`;
+
+const TokenSelector = ({ close, onTokenSelect, chain }) => {
+    const [selectedChain, setSelectedChain] = useState(chain);
+    return (
+        <TokenSelectorContainer>
+            <CloseButton onClick={close} src={crossLogo}></CloseButton>
+            <Heading>Select chain</Heading>
+            <ChainSelectorContainer>
+                {Object.entries(faucetTokens.chains).map(
+                    ([key, chain], index) => (
+                        <Chain
+                            onClick={() => setSelectedChain(key)}
+                            selected={selectedChain == key}
+                        >
+                            <ChainImage src={chain.image}></ChainImage>
+                            <ChainName>{chain.name}</ChainName>
+                        </Chain>
+                    )
+                )}
+            </ChainSelectorContainer>
+            <Heading style={{ marginTop: '4%' }}>Select token</Heading>
+            {Object.entries(faucetTokens.tokens)
+                .filter(([key, token]) =>
+                    token.chainIds.includes(
+                        faucetTokens.chains[selectedChain].chainId
+                    )
+                )
+                .map(([key, token], index) => (
+                    <TokeRow
+                        onClick={() => {
+                            onTokenSelect({ chain: selectedChain, token: key });
+                        }}
+                        live={token.live}
+                    >
+                        <TokenImage src={token.image}></TokenImage>
+                        <TokenName>{token.symbol}</TokenName>
+                        {!token.live && (
+                            <TokenComingSoon>Coming soon</TokenComingSoon>
+                        )}
+                    </TokeRow>
+                ))}
+        </TokenSelectorContainer>
+    );
+};
+
+export default TokenSelector;
